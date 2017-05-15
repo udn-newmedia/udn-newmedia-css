c23ddf55aef81cd89d2c40211c3499ccc95b56d5
Waiting7777 <waiting0819@msn.com>
2017-05-15 11:23:33 +0800

    Add quote and head

--- js/index.js
+++ js/index.js
@@ -10,10 +10,12 @@
         scroll_now = $(window).scrollTop();
 
         if(scroll_now > h){
-             $('#indicator').css('opacity', 1)
+            $('#indicator').css('opacity', 1)
+            $('#head').css('top', '10px')
         }
         else{
             $('#indicator').css('opacity', 0)
+            $('#head').css('top', '0')
         }
 
         $('#indicator-bar').css('width', scroll_now/total_height * 100 + '%');
