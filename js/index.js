5904d8eb97b2aba8f14ae48920f0cf66b3eefa91
Waiting7777 <waiting0819@msn.com>
2017-07-18 18:57:44 +0800

    add movie function

--- js/index.js
+++ js/index.js
@@ -1,5 +1,63 @@
 ($(document).ready(function(){
 
+    function moviePlay(id){
+        $('#movie-' + id).get(0).play();
+        if(progress[id - 1] == null){
+            progress[id - 1] = setInterval(function(){
+                var curTime = $('#movie-' + id).get(0).currentTime;
+                var temp = curTime / $('#movie-' + id).get(0).duration * 100;
+                if(temp > 0.6){
+                    $('.video-play[data-target="' + id + '"]').css('opacity', 0);
+                }
+                if(Math.floor(curTime/5) > movie_progress){
+                    movie_progress = Math.floor(curTime/5)
+                    ga("send", {
+                        "hitType": "event",
+                        "eventCategory": "movie play",
+                        "eventAction": "play",
+                        "eventLabel": "[" + platform + "] [" + title + "] [movie " + id + " play " + (movie_progress*5) + "]"
+                    });
+                }
+                
+                $('#progress-bar-' + id).css('width', temp + '%')
+            }, 600)
+        }
+    }
+        
+    function moviePause(id){
+        $('#movie-' + id).get(0).pause();
+        $('.video-play[data-target="' + id + '"]').css('opacity', 1);
+        if(progress[id-1]){
+            clearInterval(progress[id-1])
+            progress[id-1] = null;
+        }
+    }
+
+    function movieReplay(id){
+        $('#movie-' + id).get(0).currentTime = 0;
+        $('#movie-' + id).get(0).play();
+        $('.progress-bar').css('width', 0);
+        clearInterval(progress[id - 1])
+        progress[id - 1] = setInterval(function(){
+            var temp = $('#movie-' + id).get(0).currentTime / $('#movie-' + id).get(0).duration * 100
+            $('#progress-bar-' + id).css('width', temp + '%')
+        }, 600)
+    }
+
+    function movieVolume(id){
+        
+        if($('#movie-' + id).get(0).muted == true){
+            $('#movie-' + id).get(0).muted = false;
+            $('.volume[data-target="' + id + '"]').removeClass('fa-volume-off').addClass('fa-volume-up')
+            $('.volume-text[data-target="' + id + '"]').text('點按關聲音');
+        }
+        else{
+            $('#movie-' + id).get(0).muted = true;
+            $('.volume[data-target="' + id + '"]').removeClass('fa-volume-up').addClass('fa-volume-off')
+            $('.volume-text[data-target="' + id + '"]').text('點按開聲音');
+        }
+    }
+
     let w = $(window).width()
     let h = $(window).height()
     let scroll_now
